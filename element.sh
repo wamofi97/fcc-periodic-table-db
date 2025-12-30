93987809623ef8e659bf50ec26c3594809ef81d0
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-12-30 02:28:37 -0500

    feat: implement element lookup functionality

--- element.sh
+++ element.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  CONDITION="atomic_number=$1"
+elif [[ $1 =~ ^[A-Za-z]{1,2}$ ]]
+then
+  CONDITION="symbol='$1'"
+else
+  CONDITION="name='$1'"
+fi
+
+RESULT=$($PSQL "SELECT elements.atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius
+FROM elements
+INNER JOIN properties USING(atomic_number)
+INNER JOIN types USING(type_id)
+WHERE $CONDITION;")
+
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+IFS="|" read NUM NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
+
+echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
