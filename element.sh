24979c0949f1da3960569c728a13cca4ca86cf29
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-12-30 02:58:51 -0500

    feat: add comment 3

--- element.sh
+++ element.sh
@@ -4,6 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 #add comment 1
 #add comment 2
+#add comment 3
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
